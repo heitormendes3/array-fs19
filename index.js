f86ac8f96b95e11926073254088244de2283a1bf
heitor mendes sousa <heitor0432@digitalcollegesul.local>
2024-09-04 20:07:51 -0300

    inicio dos metodos com array

---
+++ index.js
@@ -0,0 +1,20 @@
+// 1- Criar Array
+//let listaJogos = ['890','Crash 3','Ace Combat']
+
+// 2- Alterar o último elemento pra maiúsculo
+//console.log(listaJogos[2].toUpperCase());
+
+// 3 - Alterar todos os elementos para maiúsculo
+//  let pessoas = ['AnGelo LuStosa','Juan bEnJamin','Matheus aRaNHA']
+
+// pessoas.map((item) => console.log(item.toUpperCase()))
+
+// pessoas.map(function(item) {
+//  return console.log(item.toUpperCase());
+// })
+
+// pessoas.forEach((item) => console.log(item.toUpperCase()))
+
+// 4 - Adicionar item no array
+
+
